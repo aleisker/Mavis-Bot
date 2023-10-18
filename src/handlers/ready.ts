644c82d3b92067e829b5e534155ba0ec3266891e
Aleisker Witchery <113287870+aleisker@users.noreply.github.com>
2023-10-18 10:52:29 +0200

    Update dependencies and interactions

--- src/handlers/ready.ts
+++ src/handlers/ready.ts
@@ -1,8 +1,30 @@
 import { Application } from "../interfaces/application";
 import { Handler } from "../interfaces/handler";
+import { ActivityType } from "discord.js";
+import abbreviate from 'numabbr';
+
+import { name, version } from "../../package.json";
 
 export const ready: Handler = {
 	async executeHandler(client: Application) {
+
+		let memberCount = 0;
+		client.guilds.cache.forEach(guild => {
+			memberCount += guild.memberCount;
+		})
+
+		let statuts: string[] = [
+			`Programme : ${name}@${version}`,
+			`Surveille ${client.guilds.cache.size} serveurs`,
+			`Protège ${abbreviate(memberCount)} utilisateurs`,
+			"https://zeskel.spyraling.fr"
+		]
+
+		setInterval(() => {
+			let randomiser = Math.floor(Math.random() * statuts.length);
+			client.user?.setActivity({ name: statuts[randomiser], type: ActivityType.Listening });	
+		}, 20000);
+		
 		client.getLogger().send(`Le client est maintenant connecté en tant que : ${client.user?.tag}`, 'READY');
 	},
 
