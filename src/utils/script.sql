b4bd36af9745273c629668400ac1e5062f7e73a0
Aleisker Witchery <113287870+aleisker@users.noreply.github.com>
2023-11-08 00:01:09 +0100

    Update Database

--- src/utils/script.sql
+++ src/utils/script.sql
@@ -7,13 +7,12 @@
 DROP TABLE IF EXISTS Modules;
 CREATE TABLE IF NOT EXISTS Modules (
     ServerId VARCHAR(18) NOT NULL,
-    ModuleAr BOOLEAN NOT NULL DEFAULT false,
     ModuleMo BOOLEAN NOT NULL DEFAULT false,
     ModuleLv BOOLEAN NOT NULL DEFAULT false,
     ModuleEc BOOLEAN NOT NULL DEFAULT false,
     ModuleTk BOOLEAN NOT NULL DEFAULT false,
     ModuleWc BOOLEAN NOT NULL DEFAULT false,
-    PRIMARY KEY (ServerId);
+    PRIMARY KEY (ServerId)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Création de la table contenant les ID des salons d'envoi d'informations sur le serveur
@@ -27,20 +26,41 @@ CREATE TABLE IF NOT EXISTS Channels (
     SkippedChannel1 VARCHAR(18) NOT NULL DEFAULT 'Indéfini',
     SkippedChannel2 VARCHAR(18) NOT NULL DEFAULT 'Indéfini',
     SkippedChannel3 VARCHAR(18) NOT NULL DEFAULT 'Indéfini',
-    PRIMARY KEY (ServerId);
+    PRIMARY KEY (ServerId)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- =======================================
 -- PARTIE UTILISATEUR
 -- =======================================
 
-DROP TABLE IF EXISTS `Leveling`;
-CREATE TABLE IF NOT EXISTS `Leveling` (
+DROP TABLE IF EXISTS Leveling;
+CREATE TABLE IF NOT EXISTS Leveling (
     ServerId VARCHAR(18) NOT NULL,
-    MemberId VARCHAR(18) NOT NULL RELATIVE,
+    MemberId VARCHAR(18) NOT NULL,
     UsrLevel INT NOT NULL DEFAULT 1,
-    UsrBooster CHAR(1) DEFAULT 'D',
+    UsrBooster CHAR(1) NOT NULL DEFAULT 'D',
     UsrXpFarmed INT NOT NULL DEFAULT 0,
     UsrXpNeeded INT NOT NULL DEFAULT 200,
+    PRIMARY KEY (ServerId, MemberId)
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS Economy;
+CREATE TABLE IF NOT EXISTS Economy (
+    ServerId VARCHAR(18) NOT NULL,
+    MemberId VARCHAR(18) NOT NULL,
+    UsrMoney INT NOT NULL DEFAULT 5000,
+    UsrBank INT NOT NULL DEFAULT 0,
+    UsrCasino INT NOT NULL DEFAULT 5,
+    PRIMARY KEY (ServerId, MemberId)
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS Moderation;
+CREATE TABLE IF NOT EXISTS Moderation (
+    ServerId VARCHAR(18) NOT NULL,
+    MemberId VARCHAR(18) NOT NULL,
+    Bans INT NOT NULL DEFAULT 0,
+    Kick INT NOT NULL DEFAULT 0,
+    Mute INT NOT NULL DEFAULT 0,
+    Warn INT NOT NULL DEFAULT 0,
     PRIMARY KEY (ServerId, MemberId)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/utils/tables.ts
+++ src/utils/tables.ts
@@ -1,26 +1,50 @@
 type TinyInt = 1 | 0 | boolean;
 
 export default interface Tables {
-	Staffs: {
-		StaffId: string;
-		SPLevel: number;
-		SPPoints: number;
-		SPRequired: number;
-		MOLevel: number;
-		MOPoints: number;
-		MORequired: number;
-		ARLevel: number;
-		ARPoints: number;
-		ARRequired: number;
-		TKClaimed: number;
-		TUAmount: number;
-		TULevel: number;
-		TURequired: number;
-		TDAmount: number;
+
+	//Server Side
+
+	Modules: {
+		ServerId: string;
+		ModuleMo: TinyInt;
+		ModuleLv: TinyInt;
+		ModuleEc: TinyInt;
+		ModuleTk: TinyInt;
+		ModuleWc: TinyInt;
 	};
-	Tickets: {
-		TicketId: string;
-		OwnerId: TinyInt;
-		Handled: TinyInt;
+	Channels: {
+		ServerId: string;
+		WelcomeChannel: string;
+		GoodbyeChannel: string;
+		RankupsChannel: string;
+		SkippedChannel1: string;
+		SkippedChannel2: string;
+		SkippedChannel3: string;
 	};
-}
+
+	//Client Side
+
+	Leveling: {
+		ServerId: string;
+		MemberId: string;
+		UsrLevel: number;
+		UsrBooster: string;
+		UsrXpFarmed: number;
+		UsrXpNeeded: number;
+	};
+	Economy: {
+		ServerId: string;
+		MemberId: string;
+		UsrMoney: number;
+		UsrBank: number;
+		UsrCasino: number;
+	};
+	Moderation: {
+		ServerId: string;
+		MemberId: string;
+		Bans: number;
+		Kick: number;
+		Mute: number;
+		Warn: number;
+	};
+};
