0b273e8da583f02f66b1883ace1a247add163486
Aleisker Witchery <113287870+aleisker@users.noreply.github.com>
2023-11-07 19:41:37 +0100

    Update dependencies

--- src/utils/script.sql
+++ src/utils/script.sql
@@ -1,29 +1,46 @@
-DROP TABLE IF EXISTS `Staffs`;
-CREATE TABLE IF NOT EXISTS `Staffs` (
-   ServerId VARCHAR(18) NOT NULL,
-   StaffId VARCHAR(18) NOT NULL,
-   SPLevel INT NOT NULL DEFAULT 1,
-   SPPoints INT NOT NULL DEFAULT 0,
-   SPRequired INT NOT NULL DEFAULT 5,
-   MOLevel INT NOT NULL DEFAULT 1,
-   MOPoints INT NOT NULL DEFAULT 0,
-   MORequired INT NOT NULL DEFAULT 5,
-   ARLevel INT NOT NULL DEFAULT 1,
-   ARPoints INT NOT NULL DEFAULT 0,
-   ARRequired INT NOT NULL DEFAULT 5,
-   TKClaimed INT NOT NULL DEFAULT 0,
-   TUAmount INT NOT NULL DEFAULT 0,
-   TULevel INT NOT NULL DEFAULT 1,
-   TURequired INT NOT NULL DEFAULT 10,
-   TDAmount INT NOT NULL DEFAULT 0, 
-   PRIMARY KEY(ServerId, StaffId)
+-- =======================================
+-- PARTIE SERVEUR
+-- =======================================
+
+-- Création de la table contenant les différents états des portions de programme
+
+DROP TABLE IF EXISTS Modules;
+CREATE TABLE IF NOT EXISTS Modules (
+    ServerId VARCHAR(18) NOT NULL,
+    ModuleAr BOOLEAN NOT NULL DEFAULT false,
+    ModuleMo BOOLEAN NOT NULL DEFAULT false,
+    ModuleLv BOOLEAN NOT NULL DEFAULT false,
+    ModuleEc BOOLEAN NOT NULL DEFAULT false,
+    ModuleTk BOOLEAN NOT NULL DEFAULT false,
+    ModuleWc BOOLEAN NOT NULL DEFAULT false,
+    PRIMARY KEY (ServerId);
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-DROP TABLE IF EXISTS `Tickets`;
-CREATE TABLE IF NOT EXISTS `Tickets` (
-   ServerId VARCHAR(18) NOT NULL,
-   TicketId VARCHAR(18) NOT NULL,
-   OwnerId VARCHAR(18) NOT NULL,
-   Handled VARCHAR(18) NOT NULL,
-   PRIMARY KEY(ServerId, TicketId)
+-- Création de la table contenant les ID des salons d'envoi d'informations sur le serveur
+
+DROP TABLE IF EXISTS Channels;
+CREATE TABLE IF NOT EXISTS Channels (
+    ServerId VARCHAR(18) NOT NULL,
+    WelcomeChannel VARCHAR(18) NOT NULL DEFAULT 'Indéfini',
+    GoodbyeChannel VARCHAR(18) NOT NULL DEFAULT 'Indéfini',
+    RankupsChannel VARCHAR(18) NOT NULL DEFAULT 'Indéfini',
+    SkippedChannel1 VARCHAR(18) NOT NULL DEFAULT 'Indéfini',
+    SkippedChannel2 VARCHAR(18) NOT NULL DEFAULT 'Indéfini',
+    SkippedChannel3 VARCHAR(18) NOT NULL DEFAULT 'Indéfini',
+    PRIMARY KEY (ServerId);
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- =======================================
+-- PARTIE UTILISATEUR
+-- =======================================
+
+DROP TABLE IF EXISTS `Leveling`;
+CREATE TABLE IF NOT EXISTS `Leveling` (
+    ServerId VARCHAR(18) NOT NULL,
+    MemberId VARCHAR(18) NOT NULL RELATIVE,
+    UsrLevel INT NOT NULL DEFAULT 1,
+    UsrBooster CHAR(1) DEFAULT 'D',
+    UsrXpFarmed INT NOT NULL DEFAULT 0,
+    UsrXpNeeded INT NOT NULL DEFAULT 200,
+    PRIMARY KEY (ServerId, MemberId)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
